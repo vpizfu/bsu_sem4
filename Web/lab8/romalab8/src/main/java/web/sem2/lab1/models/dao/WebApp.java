8af75cc8d069a343573b162f037fe0b2d672b0ad
Roman Kharchenko <karlmarten2001@gmail.com>
2021-06-21 02:19:56 +0300

    docs: add labs conditions

--- Web/lab8/romalab8/src/main/java/web/sem2/lab1/models/dao/WebApp.java
+++
@@ -1,8 +0,0 @@
-package web.sem2.lab1.models.dao;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class WebApp extends Application {
-}
